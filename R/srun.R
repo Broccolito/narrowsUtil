d7f6c4447da473c759fdcdf0408ae2074878a0c0
Broccolito <wanjungu@yahoo.com>
2022-10-05 20:30:58 -0700

    auto commit

--- R/srun.R
+++ R/srun.R
@@ -15,11 +15,11 @@ srun = function(command, job_name = "runner",
 JOBCOMMAND
 '
 
-  bash_template = gsub(pattern = MEMORY, replacement = memory_per_core,
+  bash_template = gsub(pattern = "MEMORY", replacement = memory_per_core,
                        bash_template) %>%
-    gsub(pattern = NCORES, replacement = n_cores) %>%
-    gsub(pattern = JOBNAME, replacement = job_name) %>%
-    gsub(pattern = JOBCOMMAND, replacement = command)
+    gsub(pattern = "NCORES", replacement = n_cores) %>%
+    gsub(pattern = "JOBNAME", replacement = job_name) %>%
+    gsub(pattern = "JOBCOMMAND", replacement = command)
 
   write(bash_template, file = paste0(job_name, ".sh"), append = FALSE)
   system(paste0("module load slurm; sbatch ", paste0(job_name, ".sh")))
